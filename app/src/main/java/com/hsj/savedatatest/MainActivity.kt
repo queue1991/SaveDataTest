18f79873cbb41181e9036d338fe4602b4da73bbc
JoHongsik <queue1991@naver.com>
2022-03-03 19:13:06 +0900

    1. saveInstanceState activity로 이동 2. saveInstacenState 설명 주석 추가

--- app/src/main/java/com/hsj/savedatatest/MainActivity.kt
+++ app/src/main/java/com/hsj/savedatatest/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.hsj.savedatatest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnClicked(view : View){
+        var nextIntent : Intent? = null
+
+        when(view.id){
+
+            R.id.btn_save_instance_state -> {
+                nextIntent = Intent(this, SavedInstanceStateTestActivity::class.java)
+            }
+
+        }
+
+        startActivity(nextIntent)
     }
 }
--- app/src/main/java/com/hsj/savedatatest/SavedInstanceStateTestActivity.kt
+++ app/src/main/java/com/hsj/savedatatest/SavedInstanceStateTestActivity.kt
@@ -1,6 +1,7 @@
 package com.hsj.savedatatest
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -17,11 +18,17 @@ import androidx.appcompat.app.AppCompatActivity
  * putLong
  * putShort
  * putParcelable (used for objects but they must implement Parcelable)
+ *
+ * onRestoreInstanceState() is called only when recreating activity after it was killed by the OS.
+ * Such situation happen when:
+ * orientation of the device changes (your activity is destroyed and recreated)
+ * there is another activity in front of yours and at some point the OS kills your activity in order to free memory (for example).
+ * Next time when you start your activity onRestoreInstanceState() will be called.
  */
 class SavedInstanceStateTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_saved_instance)
 
         if (savedInstanceState != null) {
             // Then the application is being reloaded
@@ -32,10 +39,13 @@ class SavedInstanceStateTestActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         outState.putString("message", "This is my message to be reloaded");
+        Log.d(javaClass.name,"Testsetsetset onRestoreInstanceState")
+
         super.onSaveInstanceState(outState);
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(javaClass.name,"Testsetsetset onRestoreInstanceState")
         super.onRestoreInstanceState(savedInstanceState)
     }
 }
